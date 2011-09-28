e78b3766a0ecf10c6316ab9a945e303b2bb245cf
maximz <maxim@maximzaslavsky.com>
2011-09-27 18:49:44 -0700

    Joystick code: added two-stick joystick layout; added functionality to change between joystick layouts.

--- Preliminary Testing/PrototypeVexNet.c
+++ Preliminary Testing/PrototypeVexNet.c
@@ -1,5 +1,61 @@
 // Prototype bot: VEXNet movement tester 
 
+
+
+// Define motors here: port numbers, isReversed
+#pragma config(Motor,  port2,           rightMotor,    tmotorNormal, openLoop, reversed)
+#pragma config(Motor,  port3,           leftMotor,     tmotorNormal, openLoop)
+#pragma config(Motor,  port4,           lateralMotor,     tmotorNormal, openLoop)
+#pragma config(Motor,  port5,           elevatorOneMotor,     tmotorNormal, openLoop)
+#pragma config(Motor,  port6,           elevatorTwoMotor,     tmotorNormal, openLoop)
+
+
+/*                                        ROBOT CONFIGURATION                                         *|
+|*    NOTES:                                                                                          *|
+|*    1)  Ch1 is the X axis and Ch2 is the Y axis for the RIGHT joystick.                             *|
+|*    2)  Ch3 is the Y axis and Ch4 is the X axis for the LEFT joystick.                              *|
+|*    3)  'abs(z)' is the ABSOLUTE VALUE of 'z'.                                                      */
+
+int ljoy_x;   // This is the X value of the LEFT analog stick.
+    int ljoy_y;   // This is the Y value of the LEFT analog stick.
+	
+	int rjoy_x;   // This is the X value of the RIGHT analog stick.
+	int rjoy_y;   // This is the Y value of the RIGHT analog stick.
+	
+	int threshold = 10;   // helps to eliminate 'noise' from a joystick that isn't perfectly at (0,0)
+
+task main ()
+{
+   bool isOneStickLayout = true; // Very hacky toggling (because we don't know if RobotC has support for enums and delegates) 
+  while(true) // while(1==1)
+  {
+	if(vexRT[Btn5D] == 1 && vexRT[Btn6D] == 1)
+	{
+		// Toggle isStickLayout:
+		if(isOneStickLayout)
+		{
+			isOneStickLayout = false;
+		}
+		else
+		{
+			isOneStickLayout = true;
+		}
+	}
+  
+	if(isOneStickLayout)
+	{
+		oneStickLayout();
+	}
+	else
+	{
+		twoStickLayout();
+	}
+  }
+    
+}
+
+void oneStickLayout()
+{
 /* This is for the joystick layout where the left analog stick controls robot movement and the right one controls the elevator.
 * Controls:
 
@@ -20,34 +76,7 @@
 		// No assignments
 
 */
-
-// Define motors here: port numbers, isReversed
-#pragma config(Motor,  port2,           rightMotor,    tmotorNormal, openLoop, reversed)
-#pragma config(Motor,  port3,           leftMotor,     tmotorNormal, openLoop)
-#pragma config(Motor,  port4,           lateralMotor,     tmotorNormal, openLoop)
-#pragma config(Motor,  port5,           elevatorOneMotor,     tmotorNormal, openLoop)
-#pragma config(Motor,  port6,           elevatorTwoMotor,     tmotorNormal, openLoop)
-
-
-/*                                        ROBOT CONFIGURATION                                         *|
-|*    NOTES:                                                                                          *|
-|*    1)  Ch1 is the X axis and Ch2 is the Y axis for the RIGHT joystick.                             *|
-|*    2)  Ch3 is the Y axis and Ch4 is the X axis for the LEFT joystick.                              *|
-|*    3)  'abs(z)' is the ABSOLUTE VALUE of 'z'.                                                      */
-
-task main ()
-{
-    int ljoy_x;   // This is the X value of the LEFT analog stick.
-    int ljoy_y;   // This is the Y value of the LEFT analog stick.
-	
-	int rjoy_x;   // This is the X value of the RIGHT analog stick.
-	int rjoy_y;   // This is the Y value of the RIGHT analog stick.
-	
-	int threshold = 10;   // helps to eliminate 'noise' from a joystick that isn't perfectly at (0,0)
-
-  while(true) // while(1==1)
-  {
-    ljoy_x = vexRT[Ch4];   // This is the X value of the LEFT analog stick.
+	ljoy_x = vexRT[Ch4];   // This is the X value of the LEFT analog stick.
     ljoy_y = vexRT[Ch3];   // This is the Y value of the LEFT analog stick.
 	
 	rjoy_x = vexRT[Ch1];   // This is the X value of the RIGHT analog stick.
@@ -142,6 +171,70 @@ task main ()
       motor[rightMotor] = 0;
     }
 	*/
-  }
+}
+void twoStickLayout()
+{
+	// Main wheels
+	if(abs(vexRT[Ch3]) > threshold)         // If the left joystick is greater than or less than the threshold:
+    {
+      motor[leftMotor]  = (vexRT[Ch3])/2;   // Left Joystick Y value / 2.
+    }
+    else                                    // If the left joystick is within the threshold:
+    {
+      motor[leftMotor]  = 0;                // Stop the left motor (cancel noise)
+    }
+
+    if(abs(vexRT[Ch2]) > threshold)         // If the right joystick is greater than or less than the threshold:
+    {
+      motor[rightMotor] = (vexRT[Ch2])/2;   // Right Joystick Y value / 2.
+    }
+    else                                    // If the right joystick is within the threshold:
+    {
+      motor[rightMotor] = 0;                // Stop the right motor (cancel noise)
+    }
+	
+	// Lateral motor
+	if(vexRT[Btn5U] == 1 && vexRT[Btn6U] == 1)
+	{
+		motor[lateralMotor] = 0; // If both lateral motor buttons are pressed, deactivate lateral motor.
+	}
+	else if(vexRT[Btn5U] == 1)
+	{
+		motor[lateralMotor] = 127; // Lateral motor: left
+	}
+	else if(vexRT[Btn6U] == 1)
+	{
+		motor[lateralMotor] = -127; // Lateral motor: right
+	}
+	else
+	{
+		motor[lateralMotor] = 0; // If neither lateral motor buttons are pressed, deactivate lateral motor.
+	}
+	
+	// Elevator
+	if(vexRT[Btn8UXmtr2] == 1 && vexRT[Btn8DXmtr2] == 1)
+	{
+		// If both elevator motor buttons are pressed, deactivate elevator motors.
+		motor[elevatorOneMotor] = 0;
+		motor[elevatorTwoMotor] = 0;
+	}
+	else if(vexRT[Btn8UXmtr2] == 1)
+	{
+		// Elevator motors: up
+		motor[elevatorOneMotor] = 127;
+		motor[elevatorTwoMotor] = 127;
+	}
+	else if(vexRT[Btn8DXmtr2] == 1)
+	{
+		// Elevator motors: down
+		motor[elevatorOneMotor] = -127;
+		motor[elevatorTwoMotor] = -127;
+	}
+	else
+	{
+		// If neither elevator motor buttons are pressed, deactivate elevator motors.
+		motor[elevatorOneMotor] = 0;
+		motor[elevatorTwoMotor] = 0;
+	}
 }
 //++++++++++++++++++++++++
