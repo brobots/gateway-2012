91f81bf48e6f6e908a11b658e98f2bf7faa02232
Blaine@awesomesauce.bishops.com <none@none>
2011-12-05 16:04:20 -0800

    Removed unnecessary waits

--- Preliminary Testing/PrototypeVexNet.c
+++ Preliminary Testing/PrototypeVexNet.c
@@ -211,24 +211,23 @@ void twoStickLayout()
 // This returns true if a switch was made, or false if no switch made. Thus, it can be executed whenever with no consequences.
 bool handleLayoutSwitch(leftButton, upButton) // Btn7L: one stick; Btn7U: two stick;
 {
- 	if(leftButton == 1) //if((vexRT[Btn7L] == 1))
+   	if(leftButton == 1) //if((vexRT[Btn7L] == 1))
 		{
 				isOneStickLayout = true;
 				return true;
-					wait1Msec(20);
 		}
 
 		else if (upButton == 1) // ((vexRT[Btn7U] == 1))
 	  {
-		isOneStickLayout =false;
-		return true;
-		wait1Msec(20);
-	}
-	else
-	{
-	 // Neither button pressed, so no changes.
-	  return false;
-	}
+	  	isOneStickLayout =false;
+		  return true;
+  	}
+
+  	else
+  	{
+	   // Neither button pressed, so no changes.
+	    return false;
+  	}
 }
 
 void blueAIFree()
