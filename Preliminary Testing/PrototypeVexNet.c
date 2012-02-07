b75a119b3c5e5c53dadce8866211dac53ee2aa44
maximz <maxim@maximzaslavsky.com>
2012-02-07 00:45:01 -0800

    AI controls timed
    --HG--
    branch : cleanup

--- Preliminary Testing/PrototypeVexNet.c
+++ Preliminary Testing/PrototypeVexNet.c
@@ -311,11 +311,76 @@ bool redAITrapped()
 
 void userControl()
 {
+	if (handleLayoutSwitch(vexRT[Btn7L], vexRT[Btn7U])) // combination to switch to a layout, if necessary
+	{
+		wait1Msec(20); // short reset time
+	}
 
+	if (isOneStickLayout == true)
+	{
+		oneStickLayout();
+	}
+	else
+	{
+		twoStickLayout();
+	}
 }
-void aiControl()
-{
 
+void userControlLooped()
+{
+	while (true)
+	{
+		if(!userControl())
+		{
+			break;
+		}
+	}
+}
+
+bool aiControl(left, right, up, down)
+{
+	if (left == 1)
+	{
+		if(!redAITrapped())
+		{
+			return false; // let's break
+		}
+	}
+	else if (right == 1)
+	{
+		if(!blueAITrapped())
+		{
+			return false; // let's break
+		}
+	}
+	else if (up == 1)
+	{
+		if(!redAIFree())
+		{
+			return false; // let's break
+		}
+	}
+	else if (down == 1)
+	{
+		if(!blueAIFree())
+		{
+			return false; // let's break
+		}
+	}
+	else
+	{
+		// awkward
+		return false; // back to user control, since our AI stuff didn't work.
+	}
+}
+
+void aiControlTimed(left, right, up, down)
+{
+	ClearTimer(T1);
+	while (time10[T1] < 2000)
+	{
+		aiControl(left, right, up, down);
+	}
 }
 
 /* Region: Main Task */
