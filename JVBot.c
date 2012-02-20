0864b0ff2a81776add245578b237a6005a2916b9
maximz <maxim@maximzaslavsky.com>
2012-02-20 00:27:56 -0800

    Reorganized JVBot - now it compiles

--- JVBot.c
+++ JVBot.c
@@ -17,35 +17,7 @@
 
 int threshold = 12.7;
 
-void pre_auton()
-{
-	// All activities that occur before the competition starts
-	// Example: clearing encoders, setting servo positions, ...
-
-}
-task autonomous()
-{
-  // .....................................................................................
-  // Insert user code here.
-  // .....................................................................................
-	AI();
-}
-task usercontrol()
-{
-	// User control code here, inside the loop
-
-	while (true)
-	{
-	  // This is the main execution loop for the user control program. Each time through the loop
-	  // your program should update motor + servo values based on feedback from the joysticks.
-
-	  // .....................................................................................
-	  // Insert user code here. This is where you use the joystick values to update your motors, etc.
-	  // .....................................................................................
-
-	  ManualControl();
-	}
-}
+/* Strategy methods */
 
 void AI()
 {
@@ -78,4 +50,36 @@ void ManualControl()
   }
   return;
   }
+}
+
+/* Control flow */
+
+void pre_auton()
+{
+	// All activities that occur before the competition starts
+	// Example: clearing encoders, setting servo positions, ...
+
+}
+task autonomous()
+{
+  // .....................................................................................
+  // Insert user code here.
+  // .....................................................................................
+	AI();
+}
+task usercontrol()
+{
+	// User control code here, inside the loop
+
+	while (true)
+	{
+	  // This is the main execution loop for the user control program. Each time through the loop
+	  // your program should update motor + servo values based on feedback from the joysticks.
+
+	  // .....................................................................................
+	  // Insert user code here. This is where you use the joystick values to update your motors, etc.
+	  // .....................................................................................
+
+	  ManualControl();
+	}
 }
