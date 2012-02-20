db3fdb6b3fe990daff3f9c8248d210980b0f0cc9
maximz <maxim@maximzaslavsky.com>
2012-02-20 00:26:58 -0800

    Added competition template stuff to JVBot

--- JVBot.c
+++ JVBot.c
@@ -5,10 +5,55 @@
 #pragma config(Motor,  port5,           clawmidleft,   tmotorNormal, openLoop, reversed)
 #pragma config(Motor,  port6,           clawmidright,  tmotorNormal, openLoop)
 #pragma config(Motor,  port7,           clawwheel,     tmotorNormal, openLoop)
+#pragma platform(VEX)
+
+//Competition Control and Duration Settings
+#pragma competitionControl(Competition)
+#pragma autonomousDuration(20)
+#pragma userControlDuration(120)
+#include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
+
+/* BroBots - The Bishop's School 2011-2012 Robotics Team JV Bot */
 
 int threshold = 12.7;
 
-void main()
+void pre_auton()
+{
+	// All activities that occur before the competition starts
+	// Example: clearing encoders, setting servo positions, ...
+
+}
+task autonomous()
+{
+  // .....................................................................................
+  // Insert user code here.
+  // .....................................................................................
+	AI();
+}
+task usercontrol()
+{
+	// User control code here, inside the loop
+
+	while (true)
+	{
+	  // This is the main execution loop for the user control program. Each time through the loop
+	  // your program should update motor + servo values based on feedback from the joysticks.
+
+	  // .....................................................................................
+	  // Insert user code here. This is where you use the joystick values to update your motors, etc.
+	  // .....................................................................................
+
+	  ManualControl();
+	}
+}
+
+void AI()
+{
+	return;
+}
+
+
+void ManualControl()
 {
   while (true)
   {
